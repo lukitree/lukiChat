05a8c50601711f03a8fac9dae7914d84fb3b4a20
lukitree <lukitree@gmail.com>
2015-08-12 14:13:20 -0600

    Add default values for username, port, and server address (for testing)

--- lukiChat/connectdialog.cpp
+++ lukiChat/connectdialog.cpp
@@ -4,6 +4,11 @@ ConnectDialog::ConnectDialog(QWidget *parent)
 	: QDialog(parent)
 {
 	setupUi(this);
+
+	//TESTING
+	usernameEdit->setText("NoName");
+	portEdit->setText("42069");	// default server port
+	serverEdit->setText("127.0.0.1");
 }
 
 ConnectDialog::~ConnectDialog()
